4bac1fa90cf5e541ac3d8131d87b10ab1e6acaab
GuidoFC <guidofigueroa96@gmail.com>
2024-11-17 11:25:43 +0100

    Ya tengo conexion a la base de datos

---
+++ src/main/java/PildoraInformatica/PruebasJDBC.java
@@ -0,0 +1,48 @@
+package PildoraInformatica;
+
+import JOANGALMES.MySQLConnnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PruebasJDBC {
+
+    private Connection connetion;
+    private static MySQLConnnection instance;
+
+    public PruebasJDBC() {
+        try{
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            connetion = DriverManager.getConnection("jdbc:mysql://localhost:3302/nasa", "root", "root");
+            if (connetion!=null){
+                System.out.println("Conectado con Exito a Mysql");
+            }
+        }catch (Exception e){
+            System.out.println("No funciona la conexion");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static MySQLConnnection getInstance(){
+        if (instance==null){
+            instance = new MySQLConnnection();
+        }
+        return instance;
+    }
+
+    public void desconectar(){
+        if (connetion!=null){
+            try {
+                connetion.close();
+            } catch (Exception e) {
+                System.err.println( "Error desconectado mysql " +  e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PruebasJDBC pruebasJDBC = new PruebasJDBC();
+        pruebasJDBC.desconectar();
+
+    }
+}
