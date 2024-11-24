c540d14dfd259d6f4a4ff34b9a2b4d4bfc04f137
GuidoFC <guidofigueroa96@gmail.com>
2024-11-24 12:38:37 +0100

    Ya puedo cerrar session

--- src/main/java/principal/demo/controlador/UsuarioServletCerrarSesion.java
+++ src/main/java/principal/demo/controlador/UsuarioServletCerrarSesion.java
@@ -1,31 +1,31 @@
-//package principal.demo.controlador;
-//
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//
-//import java.io.IOException;
-//
-//@WebServlet(
-//        name = "CerrarSesionServlet",
-//        urlPatterns = "/cerrarSesion"
-//)
-//public class UsuarioServletCerrarSesion extends HttpServlet {
-//
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        // Invalidar la sesión actual
-//        request.getSession().invalidate();
-//
-//        // Redirigir al usuario a la página de inicio de sesión u otra página
-//        response.sendRedirect("/AppWeb24");
-//
-//
-//    }
-//
-//
-//}
+package principal.demo.controlador;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(
+        name = "CerrarSesionServlet",
+        urlPatterns = "/cerrarSesion"
+)
+public class UsuarioServletCerrarSesion extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Invalidar la sesión actual
+        request.getSession().invalidate();
+
+        // Redirigir al usuario a la página de inicio de sesión u otra página
+        response.sendRedirect("/nasa");
+
+
+    }
+
+
+}
