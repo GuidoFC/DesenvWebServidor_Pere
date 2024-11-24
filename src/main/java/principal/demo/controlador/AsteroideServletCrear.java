2a766d3f6cef4c2c6bb5ef188e132f2207ab4d29
GuidoFC <guidofigueroa96@gmail.com>
2024-11-25 00:13:44 +0100

    Refactorizado

--- src/main/java/principal/demo/controlador/AsteroideServletCrear.java
+++ src/main/java/principal/demo/controlador/AsteroideServletCrear.java
@@ -1,30 +1,18 @@
 package principal.demo.controlador;
 
-import ObtenerJSonNasa.ChatGept;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import principal.demo.DAO.AsteroideDAOAntiguo;
-import principal.demo.model.Aproach;
 import principal.demo.model.Asteroide;
 import principal.demo.service.AsteroideService;
+import principal.demo.service.NasaApiService;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.sql.Date;
 import java.util.List;
 
 //Esta es una anotación para decirle a Java que esta clase será un "Servlet".
@@ -55,100 +43,20 @@ public class AsteroideServletCrear extends HttpServlet {
 
         } else if ("ApiNasa".equals(action)) {
 
-            SessionFactory sessionFactory;
-            final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+
             try {
-                sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
-                try {
-                    String apiKey = "pRlxWWDRqBNWpDg8WS54wEfM7lF3Gl7E6gaXAOsc";
-                    URL url = new URL("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=" + apiKey);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
+                NasaApiService nasaApiService = new NasaApiService();
+                JSONObject jsonResponse = nasaApiService.getNasaApi();
 
-                    int responseCode = connection.getResponseCode();
+                asteroideService.guardarInformacionNasaApi(jsonResponse);
 
-                    if (responseCode == 200) {
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                        StringBuilder response1 = new StringBuilder();
-                        String line;
-
-                        while ((line = reader.readLine()) != null) {
-                            response1.append(line);
-                        }
-                        reader.close();
-
-                        JSONObject jsonResponse = new JSONObject(response1.toString());
-                        JSONArray nearEarthObjects = jsonResponse.getJSONArray("near_earth_objects");
-
-                        for (int i = 0; i < nearEarthObjects.length(); i++) {
-                            JSONObject asteroid = nearEarthObjects.getJSONObject(i);
-                            Long idAsteroide = asteroid.getLong("id");
-                            String name = asteroid.getString("name");
-                            double magnitud = asteroid.getDouble("absolute_magnitude_h");
-                            boolean peligroso = asteroid.getBoolean("is_potentially_hazardous_asteroid");
-
-                            // Guardar el Asteroide
-                            Session miSession = sessionFactory.openSession();
-                            Asteroide asteroide1 = new Asteroide(idAsteroide, name, magnitud, 0, peligroso);
-                            try {
-                                miSession.beginTransaction();
-                                miSession.save(asteroide1);
-                                miSession.getTransaction().commit();
-                                System.out.println("Insert correctamente del Asteroide");
-                            } catch (Exception e) {
-                                System.out.println("Error al insertar el Asteroide");
-                                e.printStackTrace();
-                            } finally {
-                                miSession.close();
-                            }
-
-                            // Obtener y guardar Aproach
-                            JSONArray closeApproachData = asteroid.getJSONArray("close_approach_data");
-                            for (int j = 0; j < closeApproachData.length(); j++) {
-                                JSONObject approach = closeApproachData.getJSONObject(j);
-                                String closeApproachDate = approach.getString("close_approach_date");
-                                Date approachDateFormato = Date.valueOf(closeApproachDate);
-
-                                JSONObject relativeVelocity = approach.getJSONObject("relative_velocity");
-                                Float kilometersPerSecond = relativeVelocity.getFloat("kilometers_per_second");
-
-                                Session miSession1 = sessionFactory.openSession();
-                                try {
-                                    miSession1.beginTransaction();
-                                    Asteroide miAsteroide = miSession1.get(Asteroide.class, idAsteroide);
-
-                                    if (miAsteroide != null) {  // Verificar si miAsteroide no es null
-                                        Aproach miAproachDef = new Aproach(approachDateFormato, kilometersPerSecond, 14.6f, "TierraFalta", miAsteroide);
-                                        miAsteroide.agregarAproach(miAproachDef);
-                                        miSession1.save(miAproachDef);
-                                        miSession1.getTransaction().commit();
-                                        System.out.println("Insert correctamente del Aproach");
-                                    } else {
-                                        System.out.println("No se encontró el Asteroide con id: " + idAsteroide);
-                                    }
-                                } catch (Exception e) {
-                                    System.out.println("Error al insertar el Aproach");
-                                    e.printStackTrace();
-                                } finally {
-                                    miSession1.close();
-                                }
-                            }
-                        }
-                        connection.disconnect();
-                    } else {
-                        throw new Exception(responseCode + " Error: " + connection.getResponseMessage());
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    sessionFactory.close();  // Cerrar el SessionFactory al final
-                }
 
             } catch (Exception e) {
-                StandardServiceRegistryBuilder.destroy(registry);
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
+
+
             processRequest(request, response);
         } else {
             processRequest(request, response); // Mostrar la lista de películas
--- src/main/java/principal/demo/service/AsteroideService.java
+++ src/main/java/principal/demo/service/AsteroideService.java
@@ -1,11 +1,18 @@
 package principal.demo.service;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import principal.demo.DAO.AsteroideDAO;
-import principal.demo.DAO.AsteroideDaoImpl;
+import principal.demo.model.Aproach;
 import principal.demo.model.Asteroide;
 
+import java.sql.Date;
 import java.util.List;
-import java.util.Optional;
 
 public class AsteroideService {
 
@@ -41,16 +48,88 @@ public class AsteroideService {
     }
 
     public void removeAsteroide(Asteroide asteroide) {
-       asteroideDao.delete(asteroide);
+        asteroideDao.delete(asteroide);
 
     }
 
     public void removeAllAsteroide(List<Asteroide> listaAsteroide) {
-        for (Asteroide asteroide : listaAsteroide){
+        for (Asteroide asteroide : listaAsteroide) {
             asteroideDao.delete(asteroide);
         }
 
 
+    }
+
+    public void guardarInformacionNasaApi(JSONObject jsonResponse) {
+        SessionFactory sessionFactory;
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        try {
+
+
+            JSONArray nearEarthObjects = jsonResponse.getJSONArray("near_earth_objects");
+            for (int i = 0; i < nearEarthObjects.length(); i++) {
+                JSONObject asteroid = nearEarthObjects.getJSONObject(i);
+                Long idAsteroide = asteroid.getLong("id");
+                String name = asteroid.getString("name");
+                double magnitud = asteroid.getDouble("absolute_magnitude_h");
+                boolean peligroso = asteroid.getBoolean("is_potentially_hazardous_asteroid");
+
+                // Guardar el Asteroide
+                Session miSession = sessionFactory.openSession();
+                Asteroide asteroide1 = new Asteroide(idAsteroide, name, magnitud, 0, peligroso);
+                try {
+                    miSession.beginTransaction();
+                    miSession.save(asteroide1);
+                    miSession.getTransaction().commit();
+                    System.out.println("Insert correctamente del Asteroide");
+                } catch (Exception e) {
+                    System.out.println("Error al insertar el Asteroide");
+                    e.printStackTrace();
+                } finally {
+                    miSession.close();
+                }
+
+                // Obtener y guardar Aproach
+                JSONArray closeApproachData = asteroid.getJSONArray("close_approach_data");
+                for (int j = 0; j < closeApproachData.length(); j++) {
+                    JSONObject approach = closeApproachData.getJSONObject(j);
+                    String closeApproachDate = approach.getString("close_approach_date");
+                    Date approachDateFormato = Date.valueOf(closeApproachDate);
+
+                    JSONObject relativeVelocity = approach.getJSONObject("relative_velocity");
+                    Float kilometersPerSecond = relativeVelocity.getFloat("kilometers_per_second");
+
+                    Session miSession1 = sessionFactory.openSession();
+                    try {
+                        miSession1.beginTransaction();
+                        Asteroide miAsteroide = miSession1.get(Asteroide.class, idAsteroide);
+
+                        if (miAsteroide != null) {  // Verificar si miAsteroide no es null
+                            Aproach miAproachDef = new Aproach(approachDateFormato, kilometersPerSecond, 14.6f, "TierraFalta", miAsteroide);
+                            miAsteroide.agregarAproach(miAproachDef);
+                            miSession1.save(miAproachDef);
+                            miSession1.getTransaction().commit();
+                            System.out.println("Insert correctamente del Aproach");
+                        } else {
+                            System.out.println("No se encontró el Asteroide con id: " + idAsteroide);
+
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println("Error al insertar el Aproach");
+                        e.printStackTrace();
+                    } finally {
+                        miSession1.close();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException(e);
+        }finally {
+            sessionFactory.close();  // Cerrar el SessionFactory al final
+        }
     }
 
     public void crearAsteroide(Asteroide asteroide) {
