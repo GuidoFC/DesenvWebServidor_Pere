c18fb2b28769b2db8056c75a9c59e5dc6e44e095
GuidoFC <guidofigueroa96@gmail.com>
2024-11-24 23:12:15 +0100

    Funciona el interceptor

--- src/main/java/principal/demo/interceptor/UsuarioInterceptor.java
+++ src/main/java/principal/demo/interceptor/UsuarioInterceptor.java
@@ -1,45 +1,6 @@
 package principal.demo.interceptor;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
-
-@WebFilter(servletNames = {"UsuarioServletCrearUsuario", "UsuarioServletLogin"})
-public class UsuarioInterceptor implements Filter{
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        System.out.println("Estoy en Usuario Interceptor");
-        System.out.println("Impedimos que si ha iniciado sesion, vuelva a iniciar sesion o cree un nuevo usuario");
-
-
-        HttpSession session = httpRequest.getSession(false);
-        boolean loggedIn = (session != null && session.getAttribute("email") != null);
-        String emailCapturado = (String) session.getAttribute("email");
-        System.out.println("El email capturado es " + emailCapturado);
-
-        // Si no está autenticado, redirigir a la página de inicio de sesión
-        if (loggedIn) {
-            // TODO cuando uso el sendRedirect() tengo que poner la ruta sin la "/"
-            httpResponse.sendRedirect("/nasa");
-        } else {
-            chain.doFilter(request, response);
-        }
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-    }
+public class UsuarioInterceptor {
+    // TODO solo las usuarios con session iniciada pueden entrar en la pagian
+    // TODO como hago si la pagina se hace mas grande, tendria que poner interceptor por todo??
 }
