01c200278081f6966ca97e2ebc0a591292df6e3a
GuidoFC <guidofigueroa96@gmail.com>
2024-11-26 11:14:45 +0100

    Limpiando clases que no uso

--- src/main/java/principal/demo/interceptor/UsuarioInterceptor.java
+++
@@ -1,6 +0,0 @@
-package principal.demo.interceptor;
-
-public class UsuarioInterceptor {
-    // TODO solo las usuarios con session iniciada pueden entrar en la pagian
-    // TODO como hago si la pagina se hace mas grande, tendria que poner interceptor por todo??
-}
