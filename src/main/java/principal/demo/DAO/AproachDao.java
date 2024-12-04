596317a182fb305b5135f03370240992a5e934bf
GuidoFC <guidofigueroa96@gmail.com>
2024-12-04 16:04:56 +0100

    Mirar si funciona el merge

--- src/main/java/principal/demo/DAO/AproachDao.java
+++ src/main/java/principal/demo/DAO/AproachDao.java
@@ -1,7 +1,7 @@
 package principal.demo.DAO;
 
 import principal.demo.model.Aproach;
-
+// funciona?
 public interface AproachDao extends CrudDao<Aproach, Long> {
     // Si hacemos el extends no hace falta hacer lo de aqui abajo:
 //    List<Tipus> findAll();
